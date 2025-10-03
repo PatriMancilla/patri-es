15a5fd87f0275aa0d3e3f85cf58e7f19302217c1
Patricia <78591737+patri-es@users.noreply.github.com>
2025-10-03 18:53:16 +0200

    profile view and work component completed
    added styles, view and component to show the datas from a json. First example, need to improved it

--- src/components/work.vue
+++ src/components/work.vue
@@ -1,35 +1,46 @@
 <script setup>
 defineProps({
-  role: String,
-  descripcion: String,
-  duration: String,
-  current: Boolean,
-  responsabilities: Array,
-    technologies: Array
+    role: String,
+    company: String,
+    duration: String,
+    current: Boolean,
+    technologies: Array,
+    responsibilities: Array
 })
+
 </script>
 <template>
+    <div class="">
+
+    </div>
     <div class="work-item">
         <div class="work-title">
-            <h3>{{ role }}</h3>
+            <h3>{{ role }} - {{ company }}</h3>
             <div class="work-sutbitle">
                 <h5>{{ duration }}</h5>
                 <h4>{{ descripcion }}</h4>
             </div>
-            <ul class="tecnologies">
-                {{ technologies }}
+            <p class="technologies">
+                <span v-for="(item, i) in technologies" :key="i">
+                {{ item }}<span class="dark" v-if="i < technologies.length - 1">&nbsp; &bull; &nbsp;</span>
+                </span>
+            </p>
+            <ul class="responsibilities">
+                <li v-for="(item, i) in responsibilities" :key="i">
+                    <span>{{ item }}</span>
+                </li>
             </ul>
-            <p>{{ responsabilities }}</p>
         </div>
         
     </div>
+
 </template>
 <style scoped>
     .work-item{
-        justify-content:first baseline;
+        justify-content: baseline;
         align-items: start;
         text-align: left;
-        padding-left: 1em;
+        padding-top: 1.5em;
         /* 
         box-sizing: content-box;
         text-align: left;
