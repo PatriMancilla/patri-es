a7f5f053c0521cf35207b7904b3d79f79d19559a
Patricia <78591737+patri-es@users.noreply.github.com>
2025-10-03 11:54:03 +0200

    adding view, first component and json data
    modifing the workflow file also by remove the step by install node 20

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -10,22 +10,16 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ title }}</h1>
-  <h2>{{ msg }}</h2>
-
-  <!-- <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/Hello.vue</code> to test HMR
-    </p>
-  </div> -->
-
-  
+  <div class="full-page">
+    <h1>{{ title }}</h1>
+    <h2>{{ msg }}</h2>
+  </div>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+    .full-page{
+        background: url('@img/background-cats.jpg') no-repeat center;
+        background-size: cover;
+        color: white;
+    }
 </style>
