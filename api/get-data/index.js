ed4d7f85621579305eb7fa6ed4b6e9da85fdac0f
Patricia <78591737+patri-es@users.noreply.github.com>
2025-10-21 17:18:38 +0200

    update api get-data

--- api/get-data/index.js
+++ api/get-data/index.js
@@ -2,7 +2,7 @@
 module.exports = async function (context, req) {
   context.res = {
     status: 200,
-    body: { message: "✅ API funcionando correctamente desde Azure Static Web App" }
+    body: { message: "✅ API of Azure Static Web App to get data" }
   };
 };
 
